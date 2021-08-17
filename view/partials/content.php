038558f89fb567673dcd34f027e3503104b63883
juanmisena <jdbetancourth22@misena.edu.co>
2021-08-16 21:51:25 -0500

    cambios en el proyecto

--- view/partials/content.php
+++ view/partials/content.php
@@ -117,17 +117,17 @@
 
                             <!-- product -->
                             <!--juan comento todo el product pa verficar con una cola card desde la line 118 al 228-->
-                            <!-- <div class="product">
+                             <div class="product">
                                 <div class="product-img">
-                                    <img src="./img/product02.png" alt="">
+                                    <img src="./img/product02.png" alt="" class="img">
                                     <div class="product-label">
                                         <span class="new">NEW</span>
                                     </div>
                                 </div>
                                 <div class="product-body">
                                     <p class="product-category">Category</p>
-                                    <h3 class="product-name"><a href="#">product name goes here</a></h3>
-                                    <h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
+                                    <h3 class="product-name"><a href="#">product name goes here now</a></h3>
+                                    <h4 class="product-price">$980.00 <!-- <del class="product-old-price">$990.00</del> --></h4>
                                     <div class="product-rating">
                                         <i class="fa fa-star"></i>
                                         <i class="fa fa-star"></i>
@@ -142,9 +142,9 @@
                                     </div>
                                 </div>
                                 <div class="add-to-cart">
-                                    <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+                                    <button class="add-to-cart-btn button"><i class="fa fa-shopping-cart"></i> add to cart</button>
                                 </div>
-                            </div> -->
+                            </div>
                             <!-- /product -->
 
                             <!-- product -->
