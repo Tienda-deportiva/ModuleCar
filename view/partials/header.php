306093f5b011a396a4be01bf5f4a46bd9f4081cc
juanmisena <jdbetancourth22@misena.edu.co>
2021-08-19 16:14:25 -0500

    cambios en el proyecto

--- view/partials/header.php
+++ view/partials/header.php
@@ -67,7 +67,7 @@
                                             <div class="bg-light p-4 rounded-lg m-4 mt-3">
             <h3 style="margin-left: 20px">Lista del Carrito</h3>
         </div>
-        <form action="<?php echo getUrl("Factura","Factura","getInsert"); ?>" method="post">
+        <form action="<?php //echo getUrl("Factura","Factura","getInsert"); ?>" method="post">
             <div class="row">
                 <div class="col-md-4" style="margin-left: 20px">
                     <label for="my-input" style="margin-left: 20px">Mis Productos</label>
@@ -97,7 +97,7 @@
                             <h3 class="itemCartTotal">Total: 0</h3>
                         </div>
                         <div class="col d-flex justify-content-end" style="padding-left: 15px;">
-                            <button class="btn btn-success">Comprar</button>
+                            <input type="submit" value="Comprar" class="btn btn-success">
                         </div>
                     </div> 
                 </div>
--- web/js/app.js
+++ web/js/app.js
@@ -15,7 +15,7 @@ function addToCarritoItem(e){
         title: itemTitle,
         price: itemPrice,
         img: itemImg,
-        amount: 1
+        amount: 1 //cantidad -> al que usuario inserta automaticamente se convierte en 1
     }
     addItemCarrito(newItem);
 }
@@ -39,10 +39,10 @@ function renderCarrito(){
         const tr=document.createElement('tr');
         tr.classList.add('ItemCarrito');
         const Content=`
-        <td class="table__productos"><img src=${item.img} alt="" class="img" style="width: 60px"></td>
-        <td><p class="product-name">${item.title}</p></td>
-        <td><input type="number" style="width: 50px" min="1" name="cant_prod" id="cant" class="input__elemento table__cantidad" value=${item.amount}></td>
-        <td>${item.price}</td>
+        <td class="table__productos"><img src=${item.img} alt="" class="img" style="width: 60px;" name=""></td>
+        <td><input type="text" class="product-name" readonly>${item.title}</td>
+        <td><input type="number" style="width: 50px" min="1" name="cant_vendida" id="cant" class="input__elemento table__cantidad" value=${item.amount}></td>
+        <td><input type="number" readonly>${item.price}</td>
         <td><button type="button" class="btn btn-danger delete"><i class="fa fa-trash"></i></button></td>`;
         tr.innerHTML=Content;
         tbody.append(tr);
